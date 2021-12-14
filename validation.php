277f59243da0b015ffe452e5ab726b6b98d31e0a
Yesssssss-cloud <72248514+Yesssssss-cloud@users.noreply.github.com>
2021-12-14 14:09:07 +0100

    Update validation.php

--- validation.php
+++ validation.php
@@ -1,4 +1,17 @@
 <?php
+
+$servername = "localhost:8080/phpmyadmin";
+$username = "root";
+$password = "";
+$dbname = "ikea";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
 include 'dbincludes.php';
 
 session_start();
